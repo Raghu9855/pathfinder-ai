bf4690ecbc14567a0081364382ca672e6bb62758
Raghu <raghuraghu9855@email.com>
2026-03-02 14:52:02 +0530

    feat: Initialize Google Analytics and set up core application routing with protected routes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from "react-ga4";
 
 import Layout from './components/layout/Layout';
 import RegisterPage from "./pages/RegisterPage";
@@ -16,10 +17,25 @@ import QuestionDetailPage from "./pages/QuestionDetailPage";
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/features/auth/ProtectedRoute';
 
+// Initialize Google Analytics
+const TRACKING_ID = "G-CRM5SCCY3X";
+ReactGA.initialize(TRACKING_ID);
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <AnalyticsTracker />
         <Routes>
           {/* Protected Routes (Wrapped in Layout with Navbar) */}
           <Route element={<Layout />}>
